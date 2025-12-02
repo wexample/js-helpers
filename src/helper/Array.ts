03529ab19f781d0b89a86456129794632cebba46
weeger <romain.weeger@gmail.com>
2025-12-02 09:10:49 +0100

    New version

--- src/helper/Array.ts
+++ src/helper/Array.ts
@@ -1,5 +1,5 @@
 export function arrayDeleteItem<T>(haystack: T[], needle: T): T[] {
-  return haystack.filter(item => item !== needle);
+  return haystack.filter((item) => item !== needle);
 }
 
 export function arrayDeleteIndex<T>(haystack: T[], index: number): T[] {
