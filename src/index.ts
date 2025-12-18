cb66b477d6262ff811eed75fdc3b2effa886d3ef
weeger <romain.weeger@gmail.com>
2025-12-18 17:01:47 +0100

    Config

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export {stringToKebab} from './helper/string.js';
-export {mergeDeep} from './helper/object.js';
-export {COLORS, color, logTitle, logPath, logEntry} from './helper/log.js';
-export {stringToKebab as default} from './helper/string.js';
