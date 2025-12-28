85e7c85fde6a8cf6da04072c9ccc9721ff03e98e
weeger <romain.weeger@gmail.com>
2025-12-28 20:07:25 +0100

    Externalize

--- src/Helper/Time.ts
+++ src/Helper/Time.ts
@@ -1,6 +1,5 @@
-export async function sleep(ms = 0): Promise<void> {
+export async function timeSleep(ms = 0): Promise<void> {
   await new Promise<void>((resolve) => {
     setTimeout(resolve, ms);
   });
 }
-
