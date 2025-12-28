aba0fe6e2e766ad68978e6f9782286a5f24e1a2b
weeger <romain.weeger@gmail.com>
2025-12-28 20:24:47 +0100

    Externalize

--- src/Helper/Variables.ts
+++ src/Helper/Variables.ts
@@ -1,16 +1,13 @@
-export default abstract class Variables {
-  public static CLOSED = 'closed';
+export const VARIABLES = {
+  CLOSED: 'closed',
+  COMPONENT: 'component',
+  PAGE: 'page',
+  ID: 'id',
+  OPENED: 'opened',
+  PLURAL_COMPONENT: 'components',
+  PLURAL_PAGE: 'pages',
+} as const;
 
-  public static COMPONENT = 'component';
+export type VariablesValue = typeof VARIABLES[keyof typeof VARIABLES];
 
-  public static PAGE = 'page';
-
-  public static ID = 'id';
-
-  public static OPENED = 'opened';
-
-  public static PLURAL_COMPONENT = `${Variables.COMPONENT}s`;
-
-  public static PLURAL_PAGE = `${Variables.PAGE}s`;
-}
-
+export default VARIABLES;
